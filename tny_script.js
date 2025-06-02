9e720d0d241ef37faf81876f160d963776ba0337
Roman353-coder <romanks866@gmail.com>
2025-06-02 10:14:55 -0600

    Update

---
+++ tny_script.js
@@ -0,0 +1,24 @@
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 9
+   Tutorial Case
+
+   Countdown Clock
+   Author: Romanpreet kaur
+   Date:  May 29, 2025 
+
+*/
+
+// Display current date & Time
+document.getElementById("datenow").innerHTML = 'm/d/y<br />h:m:s';
+
+//Display the time left until New Years Eve Bash
+document.getElementById("days").textContent = 'dd';
+document.getElementById("hrs").textContent = 'hh';
+document.getElementById("mins").textContent = 'mm';
+document.getElementById("secs").textContent = 'ss';
+
+
+
+
+
