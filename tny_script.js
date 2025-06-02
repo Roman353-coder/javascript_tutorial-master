8c7ab7ac54fc876119ba79ac83bbc91a6be6c71c
Roman353-coder <romanks866@gmail.com>
2025-06-02 11:24:57 -0600

    Update

--- tny_script.js
+++ tny_script.js
@@ -11,12 +11,22 @@
 
 // Display current date & Time
 document.getElementById("datenow").innerHTML = 'm/d/y<br />h:m:s';
+dateStr + "<br / >" + timeStr;
 
 //Display the time left until New Years Eve Bash
 document.getElementById("days").textContent = 'dd';
 document.getElementById("hrs").textContent = 'hh';
 document.getElementById("mins").textContent = 'mm';
 document.getElementById("secs").textContent = 'ss';
+
+//Store current date & time
+var currentDay = new Date("June 2nd, 2025 11.04.45");
+var dateStr = currentDay.toLocalDatestring();
+var timeStr = currentDay.toLocaleTimeString();
+
+//Displays current date & time
+document.getElementById("dateNow").innerHTML = 
+dateStr + "<br />" + timeStr;
 
 
 
