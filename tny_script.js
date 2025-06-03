1e0016247839f7bf5ef53aa79c3b2b2d1e37f8d0
Roman353-coder <romanks866@gmail.com>
2025-06-03 11:19:59 -0600

    Update

--- tny_script.js
+++ tny_script.js
@@ -20,13 +20,20 @@ document.getElementById("mins").textContent = 'mm';
 document.getElementById("secs").textContent = 'ss';
 
 //Store current date & time
-var currentDay = new Date("June 2nd, 2025 11.04.45");
+var currentDay = new Date;
 var dateStr = currentDay.toLocalDatestring();
 var timeStr = currentDay.toLocaleTimeString();
 
 //Displays current date & time
 document.getElementById("dateNow").innerHTML = 
 dateStr + "<br />" + timeStr;
+
+//Calculate the days until New Year Bash
+var newYear = new DataTransfer('January 1st, 2025');
+var nextYear= newYear.getFullYear() + 1;
+var newYear.setFullYear(nextYear);
+var daysleft (newYear - [currentDay ] / (1000*60*60));
+
 
 
 
